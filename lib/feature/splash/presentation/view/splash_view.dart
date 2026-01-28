d9b221620fa4d11330652d48839d99912d8940e2
AhmedMagdy-10 <ahmedmagdy332002@gmail>
2026-01-28 21:35:30 +0200

    splash view UI done

--- lib/feature/splash/presentation/view/splash_view.dart
+++ lib/feature/splash/presentation/view/splash_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hup/feature/splash/presentation/widgets/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
@@ -6,6 +7,6 @@ class SplashView extends StatelessWidget {
   static const String routeName = 'splash';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: SplashViewBody());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class FruitHub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateFunction,
       initialRoute: SplashView.routeName,
     );
