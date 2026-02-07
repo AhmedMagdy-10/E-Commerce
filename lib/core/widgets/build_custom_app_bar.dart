7fc70f761850ddeccf4e579273f3dc2358af60ca
AhmedMagdy-10 <ahmedmagdy332002@gmail>
2026-02-07 11:22:13 +0200

    build custom app bar

---
+++ lib/core/widgets/build_custom_app_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hup/core/utils/app_text_styles.dart';
+
+AppBar buildAppBar(BuildContext context, String title) {
+  return AppBar(
+    centerTitle: true,
+    title: Text(title, style: TextStyles.bold19),
+    leading: IconButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      icon: Icon(Icons.arrow_back_ios_new),
+    ),
+  );
+}
--- lib/feature/auth/presentation/view/login_view.dart
+++ lib/feature/auth/presentation/view/login_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hup/core/widgets/build_custom_app_bar.dart';
+import 'package:fruits_hup/feature/auth/presentation/view/widgets/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
   static const routeName = 'login';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      appBar: buildAppBar(context, 'تسجيل الدخول'),
+
+      body: LoginViewBody(),
+    );
   }
 }
