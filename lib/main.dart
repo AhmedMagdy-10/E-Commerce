258e5857f39ed90e8c5760df79e0e7145c208be4
AhmedMagdy-10 <ahmedmagdy332002@gmail>
2026-02-16 18:10:57 +0200

    add firebase service as a backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hup/core/helper/functions/on_generate_function.dart';
@@ -5,9 +6,12 @@ import 'package:fruits_hup/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_hup/core/utils/app_colors.dart';
 import 'package:fruits_hup/feature/splash/presentation/view/splash_view.dart';
 import 'package:fruits_hup/generated/l10n.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await SharedPreferencesSingleton.inti();
   runApp(const FruitHub());
 }
