15149df82e556625e75a0b5fce0ba4c16d6641ee
AhmedMagdy-10 <ahmedmagdy332002@gmail>
2026-02-20 16:12:23 +0200

    add bloc observer

--- lib/feature/auth/presentation/view/widgets/signup_view_body_bloc_consumer.dart
+++ lib/feature/auth/presentation/view/widgets/signup_view_body_bloc_consumer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hup/core/helper/functions/show_toast_state.dart';
 import 'package:fruits_hup/feature/auth/presentation/manager/signup_cubit/sign_up_cubit.dart';
 import 'package:fruits_hup/feature/auth/presentation/manager/signup_cubit/sign_up_cubit_states.dart';
 import 'package:fruits_hup/feature/auth/presentation/view/widgets/sign_up_view_body.dart';
@@ -13,16 +14,9 @@ class SignUpViewBodyBuildConsumer extends StatelessWidget {
     return BlocConsumer<SignUpCubit, SignUpCubitStates>(
       listener: (context, state) {
         if (state is SignUpErrorState) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text(state.message), backgroundColor: Colors.red),
-          );
+          showToast(text: state.message, state: ToastStates.error);
         } else if (state is SignUpSuccessState) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text('تم إنشاء الحساب بنجاح'),
-              backgroundColor: Colors.green,
-            ),
-          );
+          showToast(text: 'تم إنشاء الحساب بنجاح', state: ToastStates.success);
         }
       },
       builder: (context, state) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hup/core/helper/functions/on_generate_function.dart';
+import 'package:fruits_hup/core/services/bloc_observer.dart';
 import 'package:fruits_hup/core/services/get_it_service.dart';
 import 'package:fruits_hup/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_hup/core/utils/app_colors.dart';
@@ -12,6 +14,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupGetIt();
+  Bloc.observer = CustomBlocObserver();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await SharedPreferencesSingleton.inti();
   runApp(const FruitHub());
